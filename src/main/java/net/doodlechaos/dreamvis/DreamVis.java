00c6ed9499078c8ba9a95c3c174fdb0a8adafcdb
doodlechaos <merobbins5@gmail.com>
2024-05-26 10:13:10 -0500

    Initial commit

---
+++ src/main/java/net/doodlechaos/dreamvis/DreamVis.java
@@ -0,0 +1,16 @@
+package net.doodlechaos.dreamvis;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DreamVis implements ModInitializer {
+	public static final String MOD_ID = "dreamvis";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
